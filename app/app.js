ea8157ba000d0e3896226739b77f374241deb4f9
Jean Carlos Farias <contato@jeancf.com.br>
2019-11-18 20:11:40 -0300

    runge kutta

---
+++ app/app.js
@@ -0,0 +1,102 @@
+'use strict'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { x: 0.0, y: 1.0, fn: 'x * Math.sqrt(y)', dx: '0.1', result: [] }
+    this.handleChange = this.handleChange.bind(this)
+    this.solve = this.solve.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState(change[e.target.name] = e.target.value)
+  }
+
+  solve () {
+    this.setState({ result: RungeKutta.solve(this.state.x, this.state.y, this.state.fn) })
+  }
+
+  render () {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <div className='row'>
+              <div className='col-md-12'>
+                <h2>Runge Kutta:</h2>
+              </div>
+            </div>
+            <div className={'row'}>
+              <div className='form-group col-md-6'>
+                <div className="input-group">
+                  <div className="input-group-addon">FN:</div>
+                  <input type="text" className="form-control" name='fn' value={this.state.fn}
+                         onChange={this.handleChange}/>
+                </div>
+              </div>
+              <div className='form-group col-md-6'>
+                <div className="input-group">
+                  <div className="input-group-addon">DX:</div>
+                  <input type="text" className="form-control" name='dx' value={this.state.dx}
+                         onChange={this.handleChange}/>
+                </div>
+              </div>
+            </div>
+            <div className={'row'}>
+              <div className='form-group col-md-5'>
+                <div className="input-group">
+                  <div className="input-group-addon">X:</div>
+                  <input type="text" className="form-control" name='x' value={this.state.x}
+                         onChange={this.handleChange}/>
+                </div>
+              </div>
+              <div className='form-group col-md-5'>
+                <div className="input-group">
+                  <div className="input-group-addon">Y:</div>
+                  <input type="text" className="form-control" name='y' value={this.state.y}
+                         onChange={this.handleChange}/>
+                </div>
+              </div>
+              <div className='col-md-2'>
+                <button onClick={this.solve} className='btn btn-block btn-primary'>Resolver</button>
+              </div>
+            </div>
+
+            <div className='row'>
+              <div className='col-md-12 text-center'>
+                <table className='table table-bordered'>
+                  <thead>
+                    <tr>
+                      <th>X</th>
+                      <th>Y</th>
+                      <th>Erro</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.result.length > 0 ?
+                      this.state.result.map((row, index) => (
+                        <tr key={index}>
+                          <td><span>{row.x}</span></td>
+                          <td><span>{row.y}</span></td>
+                          <td><span>{row.error}</span></td>
+                        </tr>
+                      ))
+                      : (
+                        <tr>
+                          <td colSpan={3}><span>Nenhum resultado.</span></td>
+                        </tr>
+                      )
+                    }
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#app'))
---
+++ app/runge-kutta.js
@@ -0,0 +1,36 @@
+function rk4 (y, x, dx, fn) {
+  const k1 = dx * eval_fn(x, y, fn)
+  const k2 = dx * eval_fn(x + dx / 2.0, +y + k1 / 2.0, fn)
+  const k3 = dx * eval_fn(x + dx / 2.0, +y + k2 / 2.0, fn)
+  const k4 = dx * eval_fn(x + dx, +y + k3, fn)
+
+  return y + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
+}
+
+function eval_fn (x, y, fn) {
+  return eval(fn)
+}
+
+function error (x, y) {
+  return (((1 / 16) * (x * x + 4) * (x * x + 4)) - y).toExponential()
+}
+
+class RungeKutta {
+  static solve (x = 0.0, y = 1.0, fn, dx) {
+    let step = 0.1, steps = 0
+    const maxSteps = 101, sampleEveryN = 10
+    let result = []
+
+    while (steps < maxSteps) {
+      if (steps % sampleEveryN === 0) {
+        result.push({ x, y, error: error(x, y) })
+      }
+
+      y = rk4(y, x, step, fn)
+      x = ((x * 10) + (step * 10)) / 10
+      steps += 1
+    }
+
+    return result
+  }
+}
