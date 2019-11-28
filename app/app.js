65feca108168400d4776638549bb02f11a12edea
Jean Carlos Farias <contato@jeancf.com.br>
2019-11-28 06:27:28 -0300

    add legend

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ class App extends React.Component {
   constructor (props) {
     super(props)
 
-    this.state = { x: 0.0, y: 1.0, fn: 'x * Math.sqrt(y)', dx: 0.1, result: [] }
+    this.state = { x: 0.0, y: 1.0, fn: 'x * Math.sqrt(y)', h: 0.1, result: [] }
     this.handleChange = this.handleChange.bind(this)
     this.solve = this.solve.bind(this)
   }
@@ -21,7 +21,7 @@ class App extends React.Component {
       result: RungeKutta.solve(
         parseFloat(this.state.x),
         parseFloat(this.state.y),
-        parseFloat(this.state.dx),
+        parseFloat(this.state.h),
         this.state.fn
       )
     })
@@ -31,55 +31,60 @@ class App extends React.Component {
     return (
       <div className='container'>
         <div className='row'>
-          <div className='col-md-12'>
+          <div className='col-sm-12'>
             <div className='row'>
-              <div className='col-md-12'>
-                <h2>Runge Kutta:</h2>
+              <div className='col-sm-12'>
+                <h2>Runge Kutta 4º Ordem:</h2>
               </div>
             </div>
             <div className={'row'}>
-              <div className='form-group col-md-8'>
-                <div className="input-group">
-                  <div className="input-group-addon">FN:</div>
-                  <input type="text" className="form-control" name='fn' value={this.state.fn}
-                         onChange={this.handleChange}/>
-                </div>
-              </div>
-              <div className='form-group col-md-4'>
-                <div className="input-group">
-                  <div className="input-group-addon">DX:</div>
-                  <input type="number" className="form-control" name='dx' value={this.state.dx}
-                         onChange={this.handleChange}/>
-                </div>
-              </div>
-            </div>
-            <div className={'row'}>
-              <div className='form-group col-md-5'>
+              <div className='form-group col-sm-4'>
                 <div className="input-group">
                   <div className="input-group-addon">X:</div>
                   <input type="number" className="form-control" name='x' value={this.state.x}
                          onChange={this.handleChange}/>
                 </div>
               </div>
-              <div className='form-group col-md-5'>
+              <div className='form-group col-sm-4'>
                 <div className="input-group">
                   <div className="input-group-addon">Y:</div>
                   <input type="number" className="form-control" name='y' value={this.state.y}
                          onChange={this.handleChange}/>
                 </div>
               </div>
-              <div className='col-md-2'>
+              <div className='form-group col-sm-4'>
+                <div className="input-group">
+                  <div className="input-group-addon">H:</div>
+                  <input type="number" className="form-control" name='h' value={this.state.h}
+                         onChange={this.handleChange}/>
+                </div>
+              </div>
+            </div>
+            <div className={'row'}>
+              <div className='form-group col-sm-9 col-md-10'>
+                <div className="input-group">
+                  <div className="input-group-addon">FN:</div>
+                  <input type="text" className="form-control" name='fn' value={this.state.fn}
+                         onChange={this.handleChange}/>
+                </div>
+              </div>
+              <div className='col-sm-3 col-md-2'>
                 <button onClick={this.solve} className='btn btn-block btn-primary' type='button'>Resolver</button>
               </div>
             </div>
 
             <div className='row'>
-              <div className='col-md-12 text-center'>
-                <table className='table table-bordered'>
+              <div className='col-sm-12 text-center'>
+                <table className='table table-bordered result'>
                   <thead>
                     <tr>
+                      <th/>
                       <th>X</th>
                       <th>Y</th>
+                      <th>K1</th>
+                      <th>K2</th>
+                      <th>K3</th>
+                      <th>K4</th>
                       <th>Erro</th>
                     </tr>
                   </thead>
@@ -87,20 +92,60 @@ class App extends React.Component {
                     {this.state.result.length > 0 ?
                       this.state.result.map((row, index) => (
                         <tr key={index}>
+                          <td><span>{index}</span></td>
                           <td><span>{row.x}</span></td>
                           <td><span>{row.y}</span></td>
+                          <td><span>{row.k1}</span></td>
+                          <td><span>{row.k2}</span></td>
+                          <td><span>{row.k3}</span></td>
+                          <td><span>{row.k4}</span></td>
                           <td><span>{row.error}</span></td>
                         </tr>
                       ))
                       : (
                         <tr>
-                          <td colSpan={3}><span>Nenhum resultado.</span></td>
+                          <td colSpan={8}><span>Nenhum resultado.</span></td>
                         </tr>
                       )
                     }
                   </tbody>
                 </table>
               </div>
+            </div>
+
+            <div className='row'>
+              <fieldset>
+                <legend>Legenda:</legend>
+
+                <table className="table legend">
+                  <tbody>
+                    <tr>
+                      <td>X[i]</td>
+                      <td><math>X + (H * i)</math></td>
+                    </tr>
+                    <tr>
+                      <td>Y[i]</td>
+                      <td><math>Y[i] + (K1[i] + (2 * K2[i]) + (2 * K3[i]) + K4[i]) / 6</math></td>
+                    </tr>
+                    <tr>
+                      <td>K1[i]</td>
+                      <td><math>H * FN(X, Y[i])</math></td>
+                    </tr>
+                    <tr>
+                      <td>K2[i]</td>
+                      <td><math>H * FN(X = (X + H / 2), Y = (+Y[i] + K1[i] / 2))</math></td>
+                    </tr>
+                    <tr>
+                      <td>K3[i]</td>
+                      <td><math>H * FN(X = (X + H / 2), Y = (+Y[i] + K2[i] / 2))</math></td>
+                    </tr>
+                    <tr>
+                      <td>K4[i]</td>
+                      <td><math>H * FN(X = (X + H), Y = (+Y[i] + K3[i]))</math></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </fieldset>
             </div>
           </div>
         </div>
--- app/runge-kutta.js
+++ app/runge-kutta.js
@@ -1,10 +1,10 @@
-function rk4 (x, y, dx, fn) {
-  const k1 = dx * eval_fn(x, y, fn)
-  const k2 = dx * eval_fn(x + dx / 2.0, +y + k1 / 2.0, fn)
-  const k3 = dx * eval_fn(x + dx / 2.0, +y + k2 / 2.0, fn)
-  const k4 = dx * eval_fn(x + dx, +y + k3, fn)
+function rk4 (x, y, h, fn) {
+  const k1 = h * eval_fn(x, y, fn)
+  const k2 = h * eval_fn(x + h / 2.0, +y + k1 / 2.0, fn)
+  const k3 = h * eval_fn(x + h / 2.0, +y + k2 / 2.0, fn)
+  const k4 = h * eval_fn(x + h, +y + k3, fn)
 
-  return y + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
+  return { k1, k2, k3, k4, result: y + (k1 + (2.0 * k2) + (2.0 * k3) + k4) / 6.0 }
 }
 
 function eval_fn (x, y, fn) {
@@ -16,16 +16,20 @@ function error (x, y) {
 }
 
 class RungeKutta {
-  static solve (x = 0.0, y = 1.0, dx = 0.1, fn) {
-    let steps = 0
+  static solve (x = 0.0, y = 1.0, h = 0.1, fn) {
+    let steps = 0, rk4Result, k1 = 0, k2 = 0, k3 = 0, k4 = 0, result = []
     const maxSteps = 101
-    let result = []
 
     while (steps < maxSteps) {
-      result.push({ x, y, error: error(x, y) })
+      result.push({ x, y, k1, k2, k3, k4, error: error(x, y) })
 
-      y = rk4(x, y, dx, fn)
-      x = ((x * 10) + (dx * 10)) / 10
+      rk4Result = rk4(x, y, h, fn)
+      k1 = rk4Result.k1
+      k2 = rk4Result.k2
+      k3 = rk4Result.k3
+      k4 = rk4Result.k4
+      y = rk4Result.result
+      x = ((x * 10) + (h * 10)) / 10
       steps += 1
     }
 
